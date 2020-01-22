488786d3018ae692d1cc54578d1dc580a3999ee0
Silvia Amabilino <walfits@gmail.com>
2020-01-22 14:18:20 +0100

    Added the activity for checking what the inputs would result in.

--- app/src/main/java/com/example/timelapse/DisplayCheckedInputs.java
+++ app/src/main/java/com/example/timelapse/DisplayCheckedInputs.java
@@ -2,7 +2,10 @@ package com.example.timelapse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DisplayCheckedInputs extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class DisplayCheckedInputs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_checked_inputs);
+
+        // Get the Intent that started this activity and extract the message string
+        Bundle extras = getIntent().getExtras();
+        String message = extras.getString("CHECKED_INPUT_MSG");
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void confirmInputs(View view){
+
     }
 }
