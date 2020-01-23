3ced59868d520120b07b74c9287df5e2ddc123ac
Silvia Amabilino <walfits@gmail.com>
2020-01-23 17:14:52 +0100

    Now pictures can be taken and saved

--- app/src/main/java/com/example/timelapse/DisplayCheckedInputs.java
+++ app/src/main/java/com/example/timelapse/DisplayCheckedInputs.java
@@ -1,11 +1,22 @@
 package com.example.timelapse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 public class DisplayCheckedInputs extends AppCompatActivity {
 
@@ -15,8 +26,13 @@ public class DisplayCheckedInputs extends AppCompatActivity {
         setContentView(R.layout.activity_display_checked_inputs);
 
         // Get the Intent that started this activity and extract the message string
+        String message = null;
         Bundle extras = getIntent().getExtras();
-        String message = extras.getString("CHECKED_INPUT_MSG");
+        try {
+            message = extras.getString("CHECKED_INPUT_MSG");
+        } catch (NullPointerException ex) {
+            message = "Nothing to display";
+        }
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
@@ -24,7 +40,56 @@ public class DisplayCheckedInputs extends AppCompatActivity {
     }
 
     public void confirmInputs(View view){
-        Intent intent = new Intent(this, StartTakingPhotos.class);
-        startActivity(intent);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.print("Hello\n");
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                Uri photoURI = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        photoFile
+                );
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+                galleryAddPic();
+            }
+        }
+    }
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        this.currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(this.currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
